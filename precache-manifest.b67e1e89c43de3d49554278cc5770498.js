ec6516f2fd271eac348a8868d1760847dac94b02
OH-JOO-YEONG <brb1111@naver.com>
2020-08-24 15:35:30 +0900

    Updates

---
+++ precache-manifest.b67e1e89c43de3d49554278cc5770498.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2380cb347ad909e37da4d13c9356b968",
+    "url": "/moive_app_basic/index.html"
+  },
+  {
+    "revision": "c9f3d3a1a42e46c87385",
+    "url": "/moive_app_basic/static/css/main.7a39a897.chunk.css"
+  },
+  {
+    "revision": "6217fbb8006c63353932",
+    "url": "/moive_app_basic/static/js/2.96609a9c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/moive_app_basic/static/js/2.96609a9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9f3d3a1a42e46c87385",
+    "url": "/moive_app_basic/static/js/main.270ade5a.chunk.js"
+  },
+  {
+    "revision": "afdd7a3f95b0f4a354e7",
+    "url": "/moive_app_basic/static/js/runtime-main.dd565a0a.js"
+  }
+]);
